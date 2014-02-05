2fcbec400beef9c9587b03d0534d8fbb9399ed00
Kevin Ali <kevin.adil.ali@gmail.com>
2014-02-05 04:31:25 -0500

    Photogur

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,56 @@
+class PicturesController < ApplicationController
+
+def index
+	 @most_recent_pictures = Picture.most_recent_five
+end
+
+def show
+   @picture = Picture.find(params[:id])
+end
+
+
+def new
+	@picture = Picture.new
+end
+
+
+def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      # if the save for the picture was successful, go to index.html.erb
+      redirect_to pictures_url
+    else
+      # otherwise render the view associated with the action :new (i.e. new.html.erb)
+      render :new
+    end
+end
+
+def edit
+	@picture = Picture.find(params[:id])
+end
+
+def update
+	@picture = Picture.find(params[:id])
+
+    if @picture.update_attributes(picture_params)
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+end
+
+def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_url
+  end
+
+private
+def picture_params
+  params.require(:picture).permit(:artist, :title, :url)
+end
+
+
+	
+
+end
